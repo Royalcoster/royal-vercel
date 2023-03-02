e79ccdb851afea84cb03729c75bdff6ff272c2c7
Royalcoster <cobol1962@gmail.com>
2023-03-02 07:03:26 +0100

    1

--- next.config.js
+++ next.config.js
@@ -1,26 +1,11 @@
-const path = require('path')
-const withTranslateRoutes = require('next-translate-routes/plugin');
-module.exports = withTranslateRoutes({
-  images: {
-    domains: ["cdn.shopify.com","royalcoster.com:81","royal-vercel.vercel.app"]
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/customRing',
+        permanent: true,
+      },
+    ]
   },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  reactStrictMode: true,
-  i18n: {
-    // The locales you want to support in your app
-    locales: ["en", "nl"],
-    // The default locale you want to be used when visiting a non-locale prefixed path e.g. `/hello`
-    defaultLocale: "en",
-    localeDetection: false
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    scrollRestoration: false,
-  },
-}) //I am not sure
+}
